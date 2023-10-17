66fed7f46ef840b8a4f6abb7eec6475bcf01dfb6
Chandrashekar <nagasantoshchavvakula@gmail.com>
2023-10-17 12:22:01 -0500

    Scanner package

---
+++ src/packages/first.java
@@ -0,0 +1,14 @@
+package packages;
+
+import java.util.Scanner;
+
+public class first {
+
+	public static void main(String[]args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.print("Enter user name:");
+		String userName;
+		userName=sc.nextLine();
+		System.out.print("User name is "+userName);
+	}
+}
