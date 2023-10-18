e25c91b115ab18f73a62b7927324cd776ddd63bc
Chandrashekar <nagasantoshchavvakula@gmail.com>
2023-10-17 19:33:32 -0500

    Inheritance # Vehicle & Car

---
+++ src/inheritance/Vehicle.java
@@ -0,0 +1,9 @@
+package inheritance;
+
+class Vehicle {
+	  //public String modelName = "Mustang"; 
+	  protected String brand = "Ford";        // Vehicle attribute
+	  public void honk() {                    // Vehicle method
+	    System.out.print("Brand of the car is:");
+	  }
+	}
