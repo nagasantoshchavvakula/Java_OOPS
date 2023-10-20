eb375eb8c9f3ddbf1a762c1755219c78544a874c
Chandrashekar <nagasantoshchavvakula@gmail.com>
2023-10-20 12:49:17 -0500

    added userName separately with user input

--- src/packages/first.java
+++ src/packages/first.java
@@ -7,8 +7,8 @@ public class first {
 	public static void main(String[]args) {
 		Scanner sc=new Scanner(System.in);
 		System.out.print("Enter user name:");
-		String userName;
-		userName=sc.nextLine();
+		String userName=sc.nextLine();
+		//userName=sc.nextLine();
 		System.out.print("User name is "+userName);
 	}
 }
