4278fc9cf61028f96f6be0daa64973a4c3837ee2
Chandrashekar <nagasantoshchavvakula@gmail.com>
2023-11-23 19:54:49 -0600

    Date/Time/Date&Time Format

---
+++ src/date/Main.java
@@ -0,0 +1,29 @@
+package date;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+	    LocalDate myObj = LocalDate.now(); // Create a date object
+	    System.out.println(myObj); // Display the current date
+	    
+	    LocalTime time = LocalTime.now();
+	    System.out.println(time);// Display the current time
+	    
+	    LocalDateTime dateTime = LocalDateTime.now();
+	    //System.out.println(dateTime);//Display the current date and time
+	    System.out.println("Before formatting: " + dateTime);
+	    
+	    DateTimeFormatter myFormatObj = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+
+	    String formattedDate = dateTime.format(myFormatObj);
+	    System.out.println("After formatting: " + formattedDate); //Displaying formatted date
+	    
+	  }
+
+}
