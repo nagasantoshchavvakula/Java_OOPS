a079ebcff9fef6fc2e80dc0f8da112754968f701
Chandrashekar <nagasantoshchavvakula@gmail.com>
2024-02-05 22:42:14 -0600

    example.java

--- src/example.java
+++ src/example.java
@@ -1,14 +1,26 @@
 public class example {
 
-	public static void main(String[] args) {
+
 		// TODO Auto-generated method stub
-		int i;
-		for(i=0;i<10;i++) {
-			if(i%2==0) {
-				continue;
-			}
-			System.out.println(i);
-		}	
+
+		private int i;
+		private int j;
+
+		public example(int i, int j){
+		this.i=i;
+		this.j=j;
+		}
+
+		public static void main(String[] args){
+
+		example m1=new example(10,20);
+
+		example m2=m1;
+		m2.i=111;
+		m2.j=222;
+
+		System.out.println(m1.i+" "+m1.j);
+		
 	}
 
 }
